1f028612251fa51cb83a4c0f9644e3feb7f8b86e
MichalRedm <45372892+MichalRedm@users.noreply.github.com>
2025-10-17 11:30:21 +0200

    Undo the last commit

--- Assignment 2/src/algorithms/greedy_2_regret.cpp
+++ Assignment 2/src/algorithms/greedy_2_regret.cpp
@@ -1,9 +1,8 @@
 #include "greedy_2_regret.h"
 
 #include <vector>
-#include <tuple>
 
-std::tuple<std::vector<int>, int> generate_greedy_2_regret_solution(const std::vector<PointData>& data, const std::vector<std::vector<int>>& distance_matrix, int start_node_id) {
+std::vector<int> generate_greedy_2_regret_solution(const std::vector<PointData>& data, const std::vector<std::vector<int>>& distance_matrix, int start_node_id) {
     // Placeholder implementation
-    return {{start_node_id}, 0};
+    return {start_node_id};
 }
--- Assignment 2/src/algorithms/greedy_with_weighted_sum.h
+++ Assignment 2/src/algorithms/greedy_with_weighted_sum.h
@@ -2,9 +2,8 @@
 #define GREEDY_WITH_WEIGHTED_SUM_H
 
 #include <vector>
-#include <tuple>
 #include "../core/point_data.h"
 
-std::tuple<std::vector<int>, int> generate_with_weighted_sum_solution(const std::vector<PointData>& data, const std::vector<std::vector<int>>& distance_matrix, int start_node_id);
+std::vector<int> generate_with_weighted_sum_solution(const std::vector<PointData>& data, const std::vector<std::vector<int>>& distance_matrix, int start_node_id);
 
 #endif // GREEDY_WITH_WEIGHTED_SUM_H
