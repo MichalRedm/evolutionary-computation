54c96b76c7383f65d613b1ad6f5b9aaf6e5e0735
MichalRedm <45372892+MichalRedm@users.noreply.github.com>
2025-10-24 11:17:06 +0200

    Correct directives for local search

--- Assignment 3/src/algorithms/local_search.h
+++ Assignment 3/src/algorithms/local_search.h
@@ -1,9 +1,9 @@
-#ifndef RANDOM_SOLUTION_H
-#define RANDOM_SOLUTION_H
+#ifndef LOCAL_SEARCH_H
+#define LOCAL_SEARCH_H
 
 #include <vector>
 #include "../core/point_data.h"
 
 std::vector<int> local_search(const std::vector<PointData>& data); // TODO: Adjust parameters as needed
 
-#endif // RANDOM_SOLUTION_H
+#endif // LOCAL_SEARCH_H
