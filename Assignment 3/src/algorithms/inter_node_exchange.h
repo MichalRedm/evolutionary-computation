5cbd44c3fc2ec7ba4de8d430314208e423251dd0
MichalRedm <45372892+MichalRedm@users.noreply.github.com>
2025-10-25 10:25:27 +0200

    Add docstring for inter_node_exchange

--- Assignment 3/src/algorithms/inter_node_exchange.h
+++ Assignment 3/src/algorithms/inter_node_exchange.h
@@ -5,6 +5,20 @@
 #include <math.h>
 #include "../core/point_data.h"
 
+/**
+ * @brief Calculates the change in total cost (delta) for an inter-route node exchange.
+ *
+ * This move involves replacing a node currently in the solution (`node_1`) with a node
+ * that is not in the solution (`node_2`). The function calculates the change in
+ * tour distance and node costs.
+ *
+ * @param data The vector of all point data, used to get node costs.
+ * @param distance_matrix The pre-calculated distance matrix.
+ * @param solution The current solution vector.
+ * @param node_1_position The position (index) in the solution of the node to be replaced.
+ * @param node_2_id The ID of the node from outside the solution to be inserted.
+ * @return The delta (change in cost). A negative value indicates an improvement.
+ */
 double inter_node_exchange(
     const std::vector<PointData>& data,
     std::vector<std::vector<int>>& distance_matrix,
