de1fb1fa16c62609dc652b80ae5278ee4ae1d445
MichalRedm <45372892+MichalRedm@users.noreply.github.com>
2025-10-25 10:22:43 +0200

    Add docstrings for enums

--- Assignment 3/src/algorithms/local_search.h
+++ Assignment 3/src/algorithms/local_search.h
@@ -6,24 +6,36 @@
 #include "../core/point_data.h"
 #include "../core/stagetimer.h"
 
+/**
+ * @brief Defines the type of local search algorithm to use.
+ */
 enum class SearchType {
-    STEEPEST,
-    GREEDY
+    STEEPEST, ///< Explores the entire neighborhood and makes the best possible move.
+    GREEDY    ///< Makes the first improving move it finds.
 };
 
+/**
+ * @brief Defines the type of move to use within the intra-route neighborhood.
+ */
 enum class IntraMoveType {
-    NODES_EXCHANGE,
-    EDGES_EXCHANGE
+    NODES_EXCHANGE, ///< Swaps the positions of two nodes in the solution.
+    EDGES_EXCHANGE  ///< Performs a 2-opt move by swapping two edges.
 };
 
+/**
+ * @brief Defines the method for generating the initial solution for the local search.
+ */
 enum class StartingSolutionType {
-    RANDOM,
-    GREEDY
+    RANDOM, ///< Starts from a randomly generated solution.
+    GREEDY  ///< Starts from a solution built by a greedy construction heuristic.
 };
 
+/**
+ * @brief Defines the type of neighborhood being explored.
+ */
 enum class NeighbourhoodType {
-    INTER,
-    INTRA
+    INTER, ///< Moves involving a node in the solution and a node outside of it.
+    INTRA  ///< Moves involving only nodes already in the solution.
 };
 
 std::vector<int> local_search(
