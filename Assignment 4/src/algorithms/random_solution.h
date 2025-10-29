a9f8b8001ae90508f02daf92600011beb2631759
MichalRedm <45372892+MichalRedm@users.noreply.github.com>
2025-10-29 13:23:37 +0100

    Docstring for random_solution

--- Assignment 4/src/algorithms/random_solution.h
+++ Assignment 4/src/algorithms/random_solution.h
@@ -4,6 +4,15 @@
 #include <vector>
 #include "../core/point_data.h"
 
+/**
+ * @brief Generates a random solution for the modified Traveling Salesperson Problem.
+ *
+ * This function creates a random path that visits exactly half of the nodes given in the dataset.
+ * It does this by shuffling the node indices and selecting the first half.
+ *
+ * @param data A vector of PointData objects, where each object represents a node.
+ * @return A vector of integers representing the indices of the nodes in the generated random path.
+ */
 std::vector<int> generate_random_solution(const std::vector<PointData>& data);
 
 #endif // RANDOM_SOLUTION_H
